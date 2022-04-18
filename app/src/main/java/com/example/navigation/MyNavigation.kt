b1f1a69441ce8075a2b70f8dac2ccd95c8f3ae30
Linoop Laxman <linoop1989@gmail.com>
2022-04-18 23:43:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/navigation/MyNavigation.kt
@@ -0,0 +1,67 @@
+package com.example.navigation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun MyNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = "linoop"
+                    nullable = true
+                }
+            )) { entry ->
+            DetailedScreen(name = entry.arguments?.getString("name"))
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(value = text, onValueChange = {
+            text = it
+        }, modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            navController.navigate(Screen.DetailScreen.withArgs(text))
+        }, modifier = Modifier.align(Alignment.End)) {
+            Text(text = "To detailed screen")
+        }
+    }
+}
+
+@Composable
+fun DetailedScreen(name: String?) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        Text("Hello, $name")
+    }
+}
